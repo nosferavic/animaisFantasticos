42544406e8dc4444b53d98d39c9750b41d7c92e2
victor viana <victor.deviana@outlook.com>
2024-05-15 19:58:54 -0300

    initScroll

--- script.js
+++ script.js
@@ -41,3 +41,32 @@ function initAccordion() {
   }
 }
 initAccordion();
+
+function initLinkSuave() {
+  const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+
+  if (linksInternos.length) {
+    function scrollToSection(event) {
+      event.preventDefault();
+      const href = event.currentTarget.getAttribute("href");
+      const section = document.querySelector(href);
+
+      section.scrollIntoView({
+        behavior: "smooth",
+        block: "start",
+      });
+      //forma alternativa
+
+      /*const topo = section.offsetTop
+  window.scrollTo({
+    top: topo,
+    behavior: 'smooth',
+  })*/
+    }
+
+    linksInternos.forEach((link) => {
+      link.addEventListener("click", scrollToSection);
+    });
+  }
+}
+initLinkSuave();
